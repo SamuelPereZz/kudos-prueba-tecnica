774021d6d8c8d1dfee1d9a31a1760a6a0a8bd421
Samuel Pérez <fernando.samuelperez01@gmail.com>
2024-06-28 03:32:07 -0600

    add First version of the results view

--- src/Components/ViewingResults.jsx
+++ src/Components/ViewingResults.jsx
@@ -1,9 +1,128 @@
+import { useEffect, useState } from 'react';
+import Button from './Button';
+import styled from '@emotion/styled';
+
 function ViewingResults() {
+  const [successRecords, setSuccessRecords] = useState([]);
+  const [errorRecords, setErrorRecords] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('http://localhost:5000/upload');
+        const data = await response.json();
+        if (data.ok) {
+          setSuccessRecords(data.data.success);
+          setErrorRecords(data.data.errors);
+        } else {
+          console.error('Error al obtener los datos:', data.error);
+        }
+      } catch (error) {
+        console.error('Error al conectar con la API:', error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const handleRetry = () => {
+    console.log('Reintentando la carga del registro...');
+    // lógica para reintentar la carga de registros 
+  };
+
+  const handleNewFile = () => {
+    console.log('Reiniciando la vista para cargar un nuevo archivo...');
+    // lógica para reiniciar la vista para cargar un nuevo archivo
+  };
+
   return (
-    <section>
-      Resultados de datos
-    </section>
-  )
+    <Container>
+      <h2>The {errorRecords.length} records listed below encountered errors. Please rectify these issues and retry.</h2>
+
+      <DataRows>
+        <ul>
+          {errorRecords.map((error, index) => (
+            <DataRow key={index}>
+              <Row>
+                <p>Fila:</p>
+                <p>{error.row}</p>
+              </Row>
+              <DetailsList>
+                {Object.entries(error.details).map(([field, value]) => (
+                  <Data key={field}>
+                    <span>{field}: </span>
+                    <input
+                      type='text'
+                      defaultValue={value}
+                      onChange={(e) => {
+                        console.log(`Campo ${field} editado: `, e.target.value);
+                      }}
+                    />
+                  </Data>
+                ))}
+              </DetailsList>
+              <RetryButton onClick={handleRetry}>Retry</RetryButton>
+            </DataRow>
+          ))}
+        </ul>
+      </DataRows>
+
+      <Button onClick={handleNewFile}>New File</Button>
+    </Container>
+  );
 }
 
 export default ViewingResults;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  padding: 2rem;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const DataRows = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding: 1rem;
+`;
+
+const DataRow = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  padding: 1rem;
+`;
+
+const DetailsList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+
+  li {
+    margin-bottom: 0.5rem;
+  }
+`;
+
+const Data = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  text-transform: capitalize;
+`;
+
+const RetryButton = styled(Button)`
+`;
--- src/Pages/UploadPage.jsx
+++ src/Pages/UploadPage.jsx
@@ -1,47 +1,52 @@
 import './UploadPage.css';
 import { useState } from 'react';
-import Botton1 from '../Components/Button';
-import { FaFilePen } from 'react-icons/fa6';
+import Botton1 from '../Components/Button'; 
 import ViewingResults from '../Components/ViewingResults';
+import { TiCloudStorage } from 'react-icons/ti';
+import { MdCloudDone } from 'react-icons/md';
 
 function UploadPage() {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [showResults, setShowResults] = useState(false); 
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
   };
 
   const handleClick = (event) => {
-    <ViewingResults/>
+    event.preventDefault();
+    setShowResults(true); 
   };
 
   return (
     <section>
-      <form className='file-upload-form'>
-        <label htmlFor='file' className='file-upload-label'>
-          <div className='file-upload-design'>
-            {selectedFile ? (
-              <>
-                <svg viewBox='0 0 640 512' height='1em'>
-                  <path d='M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z'></path>
-                </svg>
-                <Botton1 onClick={handleClick}>Upload File</Botton1>
-              </>
-            ) : (
-              <>
-                <FaFilePen size={'55px'} />
-                <p>Drag and Drop</p>
-                <p>or</p>
-                <Botton1 as='span'>Choose File</Botton1>
-              </>
-            )}
-          </div>
-          <input id='file' type='file' onChange={handleFileChange} />
-          <span className='filename'>
-            {selectedFile ? selectedFile.name : 'No file chosen'}
-          </span>
-        </label>
-      </form>
+      {!showResults ? (
+        <form className='file-upload-form'>
+          <label htmlFor='file' className='file-upload-label'>
+            <div className='file-upload-design'>
+              {selectedFile ? (
+                <>
+                  <MdCloudDone />
+                  <Botton1 onClick={handleClick}>Upload File</Botton1>
+                </>
+              ) : (
+                <>
+                  <TiCloudStorage />
+                  <p>Drag and Drop</p>
+                  <p>or</p>
+                  <Botton1 as='span'>Choose File</Botton1>
+                </>
+              )}
+            </div>
+            <input id='file' type='file' onChange={handleFileChange} />
+            <span className='filename'>
+              {selectedFile ? selectedFile.name : 'No file chosen'}
+            </span>
+          </label>
+        </form>
+      ) : (
+        <ViewingResults />
+      )}
     </section>
   );
 }
